a75a2a85618f37f6546119772cfa1ddf00d5ab02
pratheek reddy <kattapratheek97@gmail.com>
2020-09-01 16:42:16 +0000

    init commit

---
+++ react_app/build/precache-manifest.4da43930fcbe58742acf0eed86191884.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185bb74ce4d2d4e481db3a37e5a04b1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3804227314d9b95d8261",
+    "url": "/static/css/main.f2e7bae0.chunk.css"
+  },
+  {
+    "revision": "629207764e9f3f0a91ac",
+    "url": "/static/js/2.76bde790.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.76bde790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3804227314d9b95d8261",
+    "url": "/static/js/main.fab01c65.chunk.js"
+  },
+  {
+    "revision": "202fce87607a16d29b82",
+    "url": "/static/js/runtime-main.57e54692.js"
+  },
+  {
+    "revision": "02572deda124bc24b5da7a1817f99f1c",
+    "url": "/static/media/Bosch-Icon.02572ded.svg"
+  },
+  {
+    "revision": "0c96356792f73f1cdbb229f49a3c8c9f",
+    "url": "/static/media/Bosch-Icon.0c963567.ttf"
+  },
+  {
+    "revision": "508bfe0344ccf9afbaece53de940233f",
+    "url": "/static/media/Bosch-Icon.508bfe03.eot"
+  },
+  {
+    "revision": "f9f9a0b4e303e9a26fd47b1e1bda98a8",
+    "url": "/static/media/Bosch-Icon.f9f9a0b4.woff"
+  },
+  {
+    "revision": "5d0cb5cb43eceda3982a5ac64537ca5e",
+    "url": "/static/media/bosch.5d0cb5cb.jpg"
+  },
+  {
+    "revision": "12c077f38287e8d31d6dc3614f7a9d46",
+    "url": "/static/media/bosch_logo.12c077f3.jpg"
+  }
+]);
